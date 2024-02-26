a1a4cc18c3deb2b6952dda15ac43d8435a215751
Arkist <masteroffwuju@gmail.com>
2024-02-26 14:26:09 +0100

    added globals

---
+++ enemy.py
@@ -0,0 +1,10 @@
+from agent import Agent
+import globals
+
+
+class Enemy(Agent):
+    def __init__(self, screen, start_pos=...) -> None:
+        super().__init__(screen, start_pos)
+
+    def get_move():
+        pass
