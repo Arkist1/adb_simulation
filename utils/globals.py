7e89d01c17a880aae8ee0bb6a9901e00e14cb14a
Xander de Keijzer <Xander.dekeijzer@student.hu.nl>
2024-03-19 16:20:53 +0100

    Rework Main
    First rework for main

--- utils/globals.py
+++ utils/globals.py
@@ -33,6 +33,11 @@ class Globals:
 
     RESTART = True
     PAUSE = False
+    
+    COLOR_STAMINA = (255, 255, 10)
+    COLOR_FOOD = (90, 255, 140)
+    COLOR_HEALTH = (255, 30, 70)
+    COLOR_BAR = (75, 75, 75)
 
 
 def dist(p1: pygame.Vector2, p2: pygame.Vector2) -> float:
