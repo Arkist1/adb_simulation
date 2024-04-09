ff70aa0435535d5b2721911ab2b7d036583d795c
Xander de Keijzer <Xander.dekeijzer@student.hu.nl>
2024-04-09 11:06:16 +0200

    Fix ticks and added EntityPosUpdate
    Fixed issue with ticks for LogItems to be set when the LogItem is created instead of when it is used.
    Added the EntityPositionUpdate LogItem

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Main:
             "draw_switch": 0,
             "fps": 0,
             "pause_switch": 0,
+            "every_second": 0,
         }
 
         self.running = True
@@ -131,6 +132,13 @@ class Main:
 
         if Globals.DRAW:
             self.draw(fps)
+            
+        if self.cooldowns["every_second"] == 0:
+            self.every_second()
+            self.cooldowns["every_second"] = 1000
+            
+    def every_second(self) -> None:
+        self.logger.log(EntityPositionUpdate(self.tile_manager.players))
 
     def handle_inputs(self, dt, dt_mili) -> None:
         keys = pygame.key.get_pressed()
--- utils/logger.py
+++ utils/logger.py
@@ -3,14 +3,14 @@ import pygame
 
 class LogItem:
     def __init__(self) -> None:
-        pass
+        self.ticks = pygame.time.get_ticks()
     
     def __repr__(self) -> str:
         return str(self.dict())
     
     def dict(self) -> dict[str, any]:
         dict = {}
-        dict["ticks"] = pygame.time.get_ticks()
+        dict["ticks"] = self.ticks
         dict["class"] = str(self.__class__).split('.')[-1][:-2]
         for key, value in self.__dict__.items():
             dict[key] = value
@@ -31,48 +31,66 @@ class Logger:
     
 class ChangeSimState(LogItem):
     def __init__(self, new_state) -> None:
+        super().__init__()
         self.new_state = new_state
 
 class ChangeDrawState(LogItem):
     def __init__(self, new_state) -> None:
+        super().__init__()
         self.new_state = new_state
 
 class EntityDeath(LogItem):
     def __init__(self, entity_type: str, agent_id: int, pos: pygame.Vector2) -> None:
+        super().__init__()
         self.entity_type = entity_type
         self.agent_id = agent_id
         self.pos = [pos.x, pos.y]
 
 class EntitySpawn(LogItem):
     def __init__(self, entity_type: str, agent_id: int, pos: pygame.Vector2) -> None:
+        super().__init__()
         self.entity_type = entity_type
         self.agent_id = agent_id
         self.pos = [pos.x, pos.y]
 
 class AgentDetection(LogItem):
     def __init__(self, detection_type: str, agent_id: int, detected_id: int) -> None:
+        super().__init__()
         self.detection_type = detection_type
         self.agent_id = agent_id
         self.detected_id = detected_id
 
 class EnemyDetection(LogItem):
     def __init__(self, detection_type: str, enemy_id: int, detected_id: int) -> None:
+        super().__init__()
         self.detection_type = detection_type
         self.enemy_id = enemy_id
         self.detected_id = detected_id
 
 class ChangeCam(LogItem):
     def __init__(self, current_cam: str, new_cam: str) -> None:
+        super().__init__()
         self.current_cam = current_cam
         self.new_cam = new_cam
 
 class EnemyChangeState(LogItem):
     def __init__(self, enemy_id: int, cur_state: str, new_state: str) -> None:
+        super().__init__()
         self.enemy_id = enemy_id
         self.cur_state = cur_state
         self.new_state = new_state
         
 class ManualSpawn(LogItem):
     def __init__(self, type: str, pos: pygame.Vector2) -> None:
+        super().__init__()
         self.type = type
         self.pos = [pos.x, pos.y]
+    
+class EntityPositionUpdate(LogItem):
+    def __init__(self, entities: list) -> None:
+        super().__init__()
+        self.entities = [{
+            "class": str(entity.__class__).split('.')[-1][:-2], 
+            "agent_id": entity.__hash__(), 
+            "pos": [entity.pos.x, entity.pos.y]
+            } for entity in entities]
