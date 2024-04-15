d5ca81feaf37d7a5a14611123e935fb5f8fde4f9
Arkist <masteroffwuju@gmail.com>
2024-04-15 16:48:17 +0200

    new gitkeep

--- battlesims.py
+++ battlesims.py
@@ -14,8 +14,11 @@ dotenv.load_dotenv()
 # battles = list(combinations_with_replacement(["copycat", "cheater", "helper", "grudger", "detective"], 2) )
 battles = []
 for r in range(2, 6):
-    battles.extend(combinations(["copycat", "cheater", "helper", "grudger", "detective"], r))
+    battles.extend(
+        combinations(["copycat", "cheater", "helper", "grudger", "detective"], r)
+    )
 
+print(battles)
 SIM_AMOUNT = len(battles)
 CPU_CORES = 4
 MAX_TICKS = 1000
@@ -43,20 +46,20 @@ def run_main(inputs):
     m = Main(
         headless=RUN_HEADLESS, self_restart=False, max_ticks=MAX_TICKS, prints=False
     )
-    
+
     x0 = Globals.TILE_WIDTH / 3
     x1 = Globals.TILE_WIDTH / 3 * 2
     y0 = Globals.TILE_HEIGHT / 3
     y1 = Globals.TILE_HEIGHT / 3 * 2
-    
+
     m.single_init()
-    
+
     for p in list(m.tile_manager.players):
         m.tile_manager.remove_entity(p)
-        
+
     for p in list(m.tile_manager.enemies):
         m.tile_manager.remove_entity(p)
-    
+
     p0 = Agent(m.screen, start_pos=pygame.Vector2(x0, y0), battle_type=t0)
     p1 = Agent(m.screen, start_pos=pygame.Vector2(x1, y0), battle_type=t1)
     e0 = Enemy(m.screen, control_type="enemy", start_pos=pygame.Vector2(x0, y1))
@@ -65,7 +68,7 @@ def run_main(inputs):
     m.tile_manager.add_entity(p1)
     m.tile_manager.add_entity(e0)
     m.tile_manager.add_entity(e1)
-    
+
     m.single_start()
     m.save_logs(get_json_path(folder, id))
 
@@ -84,4 +87,3 @@ if __name__ == "__main__":
 
     with mp.Pool(CPU_CORES) as p:
         p.map(run_main, zip(range(SIM_AMOUNT), [folder] * SIM_AMOUNT))
-        
